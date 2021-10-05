54583bdd132b8b1c154646a3b6e4b50858e8ec16
XxThunderBlastxX <koustavmondal55@gmail.com>
2021-10-05 22:09:39 +0530

    done few styling

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,45 @@ class MyHomePage extends StatelessWidget {
             ),
             Column(
               children: transactions.map((tx) {
-                return Card(child: Text(tx.title.toString()));
+                return Card(
+                    child: Row(
+                  children: <Widget>[
+                    Container(
+                      child: Text(
+                        tx.amount.toString(),
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple),
+                      ),
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          tx.title.toString(),
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          tx.date.toString(),
+                          style: TextStyle(color: Colors.grey, fontSize: 13),
+                        ),
+                      ],
+                    )
+                  ],
+                ));
               }).toList(),
             ),
           ],
